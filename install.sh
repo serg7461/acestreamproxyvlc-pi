17fad479ccdfdd0aa0227c2daad3f3ff80d7487b
sgoryachkin <artful.tiger@gmail.com>
2017-09-24 23:53:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 curl -sSL https://raw.githubusercontent.com/sgoryachkin/acestreamproxyvlc-pi/master/acestream.service > /etc/systemd/system/acestream.service
 curl -sSL https://raw.githubusercontent.com/sgoryachkin/acestreamproxyvlc-pi/master/aceproxy.service > /etc/systemd/system/aceproxy.service
 curl -sSL https://raw.githubusercontent.com/sgoryachkin/acestreamproxyvlc-pi/master/vlc-nox.service > /etc/systemd/system/vlc-nox.service
-mkdir /etc/aceproxy
+mkdir -p /etc/aceproxy
 curl -sSl https://raw.githubusercontent.com/sgoryachkin/acestreamproxyvlc-pi/master/etc/torrenttv.py > /etc/aceproxy/torrenttv.py #torrent-tv acestream playlist mentioned here, http://torrent-tv.ru/c_playlist.php
 curl -sSl https://raw.githubusercontent.com/sgoryachkin/acestreamproxyvlc-pi/master/etc/p2pproxy.py > /etc/aceproxy/p2pproxy.py #torrent-tv login & passwd here
 systemctl daemon-reload
-docker pull aaaler/aceproxy-pi 
-docker pull sego/acestream-pi 
+docker pull sego/aceproxy-pi
+docker pull sego/acestream-pi
 docker pull sego/vlc-nox-pi
 systemctl enable aceproxy
 systemctl start aceproxy
